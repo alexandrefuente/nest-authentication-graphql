be8e15b60470f298a675c0066c2703b543a1ddb6
Alexandre Dy La Fuente <alexandre-fuente@hotmail.com>
2021-03-02 21:02:49 -0300

    Starting unit test of Auth

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,14 +1,30 @@
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-// import TestUtil from './../common/test/TestUtil'
+import { UserService } from '../user/user.service'
+import { User } from '../user/entity/user.entity'
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        PassportModule,
+        JwtModule.register({
+          secret: process.env.JWT_SECRET,
+          signOptions: { expiresIn: '60s' },
+        }),
+      ],
       providers: [
-        AuthService
+        UserService,
+        AuthService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {} 
+        },
       ],
     }).compile();
 
